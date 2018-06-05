0d74b8b6f8d45280739e43c5d72deb7202d130a7
wanglin3 <wanglin3@asiainfo.com>
2018-06-05 15:14:01 +0800

    添加测试自动剔除宕掉coordinator任务测试用例

---
+++ db/shell/checkHandleCoordJob.sh
@@ -0,0 +1,34 @@
+echo "test start $0"
+source `pwd`/info.conf
+
+#####tear up ####
+#check pgxc_node on coordinator cn1
+eval $cn1ConnStr -c \"select count\(*\) \
+					from pgxc_node where node_name=\'$cn2MasterName\'\"
+
+##### tear run ####
+#stop coordinator cn2
+eval $mgrConnStr -c \"stop coordinator master $cn2MasterName mode i\"
+
+#add coordinator handle job
+eval $mgrConnStr -c \"drop job cnHandle\"
+cnHandleJobStr="add job cnHandle (interval=2, command='select monitor_handle_coordinator()');"
+eval $mgrConnStr -c \"$cnHandleJobStr\"
+sleep 15
+#check pgxc_node on coordinator cn1
+eval $cn1ConnStr -c \"select count\(*\) \
+							from pgxc_node where node_name=\'$cn2MasterName\'\"
+
+#### tear down ####
+#drop job
+cnHandleJobStr="drop job cnHandle"
+eval $mgrConnStr -c \"$cnHandleJobStr\"
+#clean coordinator cn2
+eval $mgrConnStr -c \"clean coordinator master $cn2MasterName\"
+eval $mgrConnStr -c \"append coordinator master $cn2MasterName\"
+sleep 3
+#check pgxc_node on coordinator cn1
+eval $cn1ConnStr -c \"select count\(*\) from pgxc_node where node_name=\'$cn2MasterName\'\"
+
+
+echo "test stop $0"
