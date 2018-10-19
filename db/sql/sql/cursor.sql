010c4e9736379e114f92265f9fb24b5dcec6209c
wanglin3 <wanglin3@asiainfo.com>
2018-10-19 13:13:26 +0800

    修改cursor测试用例

--- db/sql/sql/cursor.sql
+++ db/sql/sql/cursor.sql
@@ -21,6 +21,23 @@ MOVE FORWARD 10 FROM c1;
 fetch 4 from c1;
 MOVE BACKWARD 100000 FROM c1;
 fetch 4 from c1;
+MOVE FORWARD ALL FROM c1;
+MOVE FORWARD -5 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD ALL FROM c1;
+MOVE BACKWARD -10000 FROM c1;
+fetch 3 from c1;
+MOVE BACKWARD 1000 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD -100 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+
 commit;
 
 -- 2
@@ -37,6 +54,22 @@ MOVE FORWARD 10 FROM c1;
 fetch 4 from c1;
 MOVE BACKWARD 100000 FROM c1;
 fetch 4 from c1;
+MOVE FORWARD ALL FROM c1;
+MOVE FORWARD -5 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD ALL FROM c1;
+MOVE BACKWARD -10000 FROM c1;
+fetch 3 from c1;
+MOVE BACKWARD 1000 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD -100 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
 commit;
 
 -- 3
@@ -53,6 +86,22 @@ MOVE FORWARD 10 FROM c1;
 fetch 4 from c1;
 MOVE BACKWARD 100000 FROM c1;
 fetch 4 from c1;
+MOVE FORWARD ALL FROM c1;
+MOVE FORWARD -5 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD ALL FROM c1;
+MOVE BACKWARD -10000 FROM c1;
+fetch 3 from c1;
+MOVE BACKWARD 1000 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD -100 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
 commit;
 
 -- 4
@@ -69,7 +118,36 @@ MOVE FORWARD 10 FROM c1;
 fetch 4 from c1;
 MOVE BACKWARD 100000 FROM c1;
 fetch 4 from c1;
+MOVE FORWARD ALL FROM c1;
+MOVE FORWARD -5 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD ALL FROM c1;
+MOVE BACKWARD -10000 FROM c1;
+fetch 3 from c1;
+MOVE BACKWARD 1000 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+MOVE BACKWARD -100 FROM c1;
+fetch -4 from c1;
+fetch 0 from c1;
+fetch 10 from c1;
+commit;
+
+
+-- 5
+delete from cursor_t1;
+begin;
+DECLARE c1  SCROLL CURSOR FOR SELECT * from cursor_t1 WHERE id1 %2 !=0 order by 1,2;
+fetch 2 from c1;
+MOVE FORWARD ALL FROM c1;
+fetch 2 from c1;
+MOVE BACKWARD ALL FROM c1;
+fetch 2 from c1;
 commit;
 
 -- drop node
 drop table cursor_t1 cascade;
+
