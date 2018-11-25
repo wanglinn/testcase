9cf7e85c88fa626d1e0d83fb3810c9c447e5ff8b
wanglin3 <wanglin3@asiainfo.com>
2018-11-25 23:37:38 +0800

    修改copy part table trigger测试用例

--- db/sql/sql/copyTrigger.sql
+++ db/sql/sql/copyTrigger.sql
@@ -95,6 +95,103 @@ select * from test_2;
 drop table test cascade;
 drop function if exists tgcptbl_func();
 
+-- testcase 5 : before insert   for each row
+
+create table test (a int, b text) partition by list (a);
+create table test_1 (b text, a int) distribute by hash(a);
+create table test_2 (b text, a int) distribute by hash(a);
+
+create or replace function tgcptbl_func() returns trigger as $$begin raise notice 'b: %', new.b; return NULL; end$$ language plpgsql;
+create trigger tg1 before insert on test_1 for each row execute procedure tgcptbl_func();
+create trigger tg2 before insert on test_2 for each row execute procedure tgcptbl_func();
+alter table test attach partition test_1 for values in (1);
+alter table test attach partition test_2 for values in (2);
+insert into test values (1, 'foo'), (2, 'bar');  -- insert none
+
+copy test from stdout;  -- copy none
+1	a
+2	b
+\.
+select * from test;
+select * from test_1;
+select * from test_2;
+drop table test cascade;
+drop function if exists tgcptbl_func();
+
+
+-- testcase 6: after insert for each row
+
+create table test (a int, b text) partition by list (a);
+create table test_1 (b text, a int) distribute by hash(a);
+create table test_2 (b text, a int) distribute by hash(a);
+
+create or replace function tgcptbl_func() returns trigger as $$begin raise notice 'b: %', new.b; return NULL; end$$ language plpgsql;
+create trigger tg1 after insert on test_1 for each row execute procedure tgcptbl_func();
+create trigger tg2 after insert on test_2 for each row execute procedure tgcptbl_func();
+alter table test attach partition test_1 for values in (1);
+alter table test attach partition test_2 for values in (2);
+insert into test values (1, 'foo'), (2, 'bar');
+
+copy test from stdout;
+1	a
+2	b
+\.
+select * from test;
+select * from test_1;
+select * from test_2;
+drop table test cascade;
+drop function if exists tgcptbl_func();
+
+
+-- testcase 7: before insert for each statement
+
+create table test (a int, b text) partition by list (a);
+create table test_1 (b text, a int) distribute by hash(a);
+create table test_2 (b text, a int) distribute by hash(a);
+
+create or replace function tgcptbl_func() returns trigger as $$begin raise notice 'b: %', new.b; return NULL; end$$ language plpgsql;
+create trigger tg1 before insert on test_1 for each statement execute procedure tgcptbl_func();
+create trigger tg2 before insert on test_2 for each statement execute procedure tgcptbl_func();
+alter table test attach partition test_1 for values in (1);
+alter table test attach partition test_2 for values in (2);
+insert into test values (1, 'foo'), (2, 'bar');  -- none notice
+
+copy test from stdout;  -- none notice
+1	a
+2	b
+\.
+select * from test;
+select * from test_1;
+select * from test_2;
+drop table test cascade;
+drop function if exists tgcptbl_func();
+
+
+-- testcase 8: after insert for each statement
+
+create table test (a int, b text) partition by list (a);
+create table test_1 (b text, a int) distribute by hash(a);
+create table test_2 (b text, a int) distribute by hash(a);
+
+create or replace function tgcptbl_func() returns trigger as $$begin raise notice 'b: %', new.b; return NULL; end$$ language plpgsql;
+create trigger tg1 after insert on test_1 for each statement execute procedure tgcptbl_func();
+create trigger tg2 after insert on test_2 for each statement execute procedure tgcptbl_func();
+alter table test attach partition test_1 for values in (1);
+alter table test attach partition test_2 for values in (2);
+insert into test values (1, 'foo'), (2, 'bar');  -- none notice
+
+copy test from stdout;  -- none notice
+1	a
+2	b
+\.
+select * from test;
+select * from test_1;
+select * from test_2;
+drop table test cascade;
+drop function if exists tgcptbl_func();
+
+
+
 -- no part table
 
 -- testcase 1      before insert
