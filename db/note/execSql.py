8f39597739ffe109eb451041cbb582c3227a0a54
wln <wanglin3@asiainfo.com>
2019-03-14 14:01:55 +0800

    Update execSql.py

--- db/note/execSql.py
+++ db/note/execSql.py
@@ -22,21 +22,43 @@ res1 = res.split('\n')
 del res1[0:2]
 del res1[-1:4]
 res1.sort()
+
 for item in res1:
    if 'coordinator' in item:
        res2 = item.split('|')
-       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1'"
-       res = os.popen(cmd,'r',1).read()
+       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1;'"
+       result = os.popen(cmd,'r',1).read()
        print "nodename : " + res2[0]
-       print res
+       print result
+
+for item in res1:
+    if 'datanode' in item:
+       res2 = item.split('|')
+       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1;'"
+       result = os.popen(cmd,'r',1).read()
+       print "nodename : " + res2[0]
+       print result
+
+print '*'*50
+
+for item in res1:
+   if 'coordinator' in item:
+       res2 = item.split('|')
+       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select count(*), min(slotid), max(slotid), \
+                  slotstatus, slotnodename  from adb_slot  group by slotnodename, slotstatus order by 2;'"
+       result = os.popen(cmd,'r',1).read()
+       if '(0 rows)' not in result:
+           print "nodename : " + res2[0]
+           print result
 
 
 for item in res1:
     if 'datanode' in item:
        res2 = item.split('|')
-       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1'" 
-       res = os.popen(cmd,'r',1).read()
-       print "nodename : " + res2[0]
-       print res 
- 
- 
+       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select count(*), min(slotid), max(slotid), \
+                  slotstatus, slotnodename  from adb_slot  group by slotnodename, slotstatus order by 2;'"
+       result = os.popen(cmd,'r',1).read()
+       if '(0 rows)' not in result:
+           print "nodename : " + res2[0]
+           print result
+
