39bfd19ecaaf4c6d0aeadd2cb754a5708eaf5059
wln <wanglin3@asiainfo.com>
2019-03-14 18:00:22 +0800

    Update execSql.py

--- db/note/execSql.py
+++ db/note/execSql.py
@@ -1,6 +1,7 @@
 列出mgr中各个node中pgxc_node中内容, 下面port = 6432是 mgr的端口号
 
-[wln@localhost1 python]$ cat execsql.py 
+[wln@localhost1 python]$ cat execsql.py
+
 #!/usr/bin/env python
 import os,sys
 port = 6432
@@ -24,41 +25,57 @@ del res1[-1:4]
 res1.sort()
 
 for item in res1:
-   if 'coordinator' in item:
-       res2 = item.split('|')
-       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1;'"
-       result = os.popen(cmd,'r',1).read()
-       print "nodename : " + res2[0]
-       print result
+    if 'coordinator' not in item:
+       continue
+
+    res2 = item.split('|')
+
+    cmd1 =  "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1;'"
+    cmd2 = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select count(*), min(slotid), \
+       max(slotid), slotstatus, slotnodename  from adb_slot  group by slotnodename, slotstatus order by 2;'"
+    cmd3 = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'show pgxc_node_name'"
+
+    cmd = cmd1
+
+    if len(sys.argv) > 1:
+        if '1' == sys.argv[1]:
+            cmd = cmd1
+        if '2' == sys.argv[1]:
+            cmd = cmd2
+        if '3' == sys.argv[1]:
+            cmd = cmd3
+
+    result = os.popen(cmd,'r',1).read()
+    print "nodename : " + res2[0]
+    print result
 
 for item in res1:
-    if 'datanode' in item:
-       res2 = item.split('|')
-       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1;'"
-       result = os.popen(cmd,'r',1).read()
-       print "nodename : " + res2[0]
-       print result
+    if 'datanode' not in item:
+       continue
 
-print '*'*50
+    res2 = item.split('|')
 
-for item in res1:
-   if 'coordinator' in item:
-       res2 = item.split('|')
-       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select count(*), min(slotid), max(slotid), \
-                  slotstatus, slotnodename  from adb_slot  group by slotnodename, slotstatus order by 2;'"
-       result = os.popen(cmd,'r',1).read()
-       if '(0 rows)' not in result:
-           print "nodename : " + res2[0]
-           print result
+    cmd1 =  "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select * from pgxc_node order by 1;'"
+    cmd2 = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select count(*), min(slotid), \
+       max(slotid), slotstatus, slotnodename  from adb_slot  group by slotnodename, slotstatus order by 2;'"
+    cmd3 = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'show pgxc_node_name'"
+
+    cmd = cmd1
+
+    if len(sys.argv) > 1:
+        if '1' == sys.argv[1]:
+            cmd = cmd1
+        if '2' == sys.argv[1]:
+            cmd = cmd2
+        if '3' == sys.argv[1]:
+            cmd = cmd3
+
+    result = os.popen(cmd,'r',1).read()
+    print "nodename : " + res2[0]
+    print result
 
 
-for item in res1:
-    if 'datanode' in item:
-       res2 = item.split('|')
-       cmd = "psql -d postgres -p " + res2[4] + " -h " + res2[1].strip() + " -c 'select count(*), min(slotid), max(slotid), \
-                  slotstatus, slotnodename  from adb_slot  group by slotnodename, slotstatus order by 2;'"
-       result = os.popen(cmd,'r',1).read()
-       if '(0 rows)' not in result:
-           print "nodename : " + res2[0]
-           print result
+
+ 
+
 
